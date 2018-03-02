2dbfa154562a79cf7dfd355964ce0e06b1379bbf
Kadawatha4 <dhanushkajayaweera4@gmail.com>
2018-03-02 22:43:49 +0530

    google roots

---
+++ second.php
@@ -0,0 +1,60 @@
+<?php
+
+
+class ParentClass{
+
+
+    public $x="public variable";
+
+
+}
+
+
+class ChildClass extends ParentClass{
+
+
+
+
+
+}
+
+
+$y=new ParentClass;
+echo $y->x;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
